4bdbcba07243fe31cb378e1e942382d64bfcf052
Facuindo <facu_nicogas@outlook.com>
2023-10-16 13:14:37 -0300

    funcion contar repeticiones

--- pi-23_5C2.py
+++ pi-23_5C2.py
@@ -17,11 +17,22 @@ def lista_aleatoria (inicio, fin):
     return lista_random
 
 
+def contar_repeticiones(lista_numeros, numero_repite):
+    se_repite = lista_numeros.count(numero_repite)
+    return se_repite
+
+
 if __name__ == '__main__':
 
     inicio = 0
-    fin = 1000
+    fin = 400
 
     lista_numeros = lista_aleatoria(inicio, fin)
-
-
+    while True:
+        try:
+            numero_repite = float(input("Cuantas veces se repite el numero: "))
+            break
+        except:
+            pass
+    se_repite = contar_repeticiones(lista_numeros, numero_repite)
+    print(f"El numero {numero_repite} se repite en la lista {se_repite} veces")
